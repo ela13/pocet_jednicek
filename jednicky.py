8af6a7f2e2814cbe93b8bc3c9e12223c1cd071d9
Michaela Nekardova <ela.nekardova@gmail.com>
2017-10-30 19:34:37 +0100

    pridavam program pocet jednicek.

---
+++ jednicky.py
@@ -0,0 +1,17 @@
+
+cislo = input('zadej cele kladne cislo: ')
+# pocet_jednicek = cislo.count('1')
+# print('pocet jednicek v cisle {} je {}.'.format(cislo, pocet_jednicek))
+
+def pocet_jednicek(cislo):
+    pocet = 0
+    while cislo > 0:
+        zbytek = cislo % 10
+        if zbytek == 1:
+            pocet = pocet + 1
+        cislo = cislo // 10
+
+    return pocet
+
+pocet = pocet_jednicek(cislo)
+print('pocet jednicek v cisle {} je {}.'.format(cislo, pocet_jednicek))
